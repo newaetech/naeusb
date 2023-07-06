b125267e9cd2990a42d84af72661511833d984ca
Alex Dewar <35226274+alex-dewar@users.noreply.github.com>
2023-07-06 16:09:40 -0300

    more luna addr changes - hopefully fixes this

--- usb_xmem.c
+++ usb_xmem.c
@@ -76,37 +76,37 @@ void exit_cs(void)
 void FPGA_setaddr(uint32_t addr)
 {
 	#if (USB_DEVICE_PRODUCT_ID == 0xACE5) || (USB_DEVICE_PRODUCT_ID == 0xC610)
-	//husky
+    //husky
 	  FPGA_ADDR_PORT->PIO_ODSR = (FPGA_ADDR_PORT->PIO_ODSR & 0x40) | (addr & 0x3F) | ((addr & 0xC0) << 1);
+    gpio_set_pin_low(FPGA_ALE_GPIO);
+    gpio_set_pin_high(FPGA_ALE_GPIO);
   #elif (USB_DEVICE_PRODUCT_ID == 0xC340)
-      smc_set_mode(SMC, 0, 0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D8,  PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D9,  PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D10, PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D11, PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D12, PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D13, PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D14, PIO_OUTPUT_0);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D15, PIO_OUTPUT_0);
-      // uint32_t odsr = FPGA_ADDR_PORT->PIO_ODSR & ~(0xFF << 10);
-      pio_set_pin_group_high(FPGA_ADDR_PORT, addr << 10);
-	#else
-			pio_sync_output_write(FPGA_ADDR_PORT, addr);
-	#endif
-
+      const uint32_t addr_pins[] = {
+        PIN_EBI_DATA_BUS_D8,
+        PIN_EBI_DATA_BUS_D9,
+        PIN_EBI_DATA_BUS_D10,
+        PIN_EBI_DATA_BUS_D11,
+        PIN_EBI_DATA_BUS_D12,
+        PIN_EBI_DATA_BUS_D13,
+        PIN_EBI_DATA_BUS_D14,
+        PIN_EBI_DATA_BUS_D15
+      };
+      for (uint8_t i = 0; i < 8; i++) {
+        gpio_configure_pin(addr_pins[i], (addr & i) ? PIO_OUTPUT_1 : PIO_OUTPUT_0);
+      }
 			gpio_set_pin_low(FPGA_ALE_GPIO);
 			gpio_set_pin_high(FPGA_ALE_GPIO);
 
-#if (USB_DEVICE_PRODUCT_ID == 0xC340)
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D8, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D9, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D10, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D11, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D12, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D13, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D14, PIN_EBI_DATA_BUS_FLAG1);
-      gpio_configure_pin(PIN_EBI_DATA_BUS_D15, PIN_EBI_DATA_BUS_FLAG1);
-#endif
+      for (uint8_t i = 0; i < 8; i++) {
+        gpio_configure_pin(addr_pins[i], PIN_EBI_DATA_BUS_FLAG1);
+      }
+	#else
+			pio_sync_output_write(FPGA_ADDR_PORT, addr);
+			gpio_set_pin_low(FPGA_ALE_GPIO);
+			gpio_set_pin_high(FPGA_ALE_GPIO);
+	#endif
+
+
 }
 #else
 void FPGA_setaddr(uint32_t addr)
