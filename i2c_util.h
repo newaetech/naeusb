3e1e468f0e2a6f4388e61f5126bdaaa005aca773
Alex Dewar <alexldewar@gmail.com>
2024-03-26 01:00:31 -0300

    clean up I2C

--- i2c_util.h
+++ i2c_util.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <stdint.h>
-extern volatile uint8_t I2C_LOCK;
+// extern volatile uint8_t I2C_LOCK;
 
 void i2c_setup(void);
 void i2c_reset(void);
 
 int i2c_write(uint8_t chip_addr, uint8_t reg_addr, void *data, uint8_t len);
 int i2c_read(uint8_t chip_addr, uint8_t reg_addr,  void *data, uint8_t len);
+
+int i2c_is_locked(void);
+int raw_i2c_read(twi_package_t *packet);
+int raw_i2c_send(twi_package_t *packet);
